363d7fd29d618ac0b2d0aa05394d77f17e2b01be
cexes <mottacesar001@gmail.com>
2022-03-29 01:56:08 -0300

    Search Bin on java, using method recursive

---
+++ binaria.java
@@ -0,0 +1,25 @@
+public class binaria {
+ 
+    public static void main(String[] args) {
+ 
+        int vetor[] = {100,200,300,400,500,600,700};
+        int fim = vetor.length -1;
+        int pivo;
+        System.out.println(BinSearch(vetor));
+
+    }
+
+
+    public static int BinSearch(int v[]){
+
+              
+     
+     
+     
+     
+        return 1;
+
+
+    }
+    
+}
