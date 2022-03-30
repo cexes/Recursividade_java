bff7d1d4983e1e498065c16e63c65822dde6e632
cexes <mottacesar001@gmail.com>
2022-03-30 02:41:53 -0300

    Recursividade: Busca Binaria em java. feitogit status

--- binaria.java
+++ binaria.java
@@ -4,22 +4,34 @@ public class binaria {
  
         int vetor[] = {100,200,300,400,500,600,700};
         int fim = vetor.length -1;
-        int pivo;
-        System.out.println(BinSearch(vetor));
-
+        int start = 0;
+        int pivo = 0;
+        int value = 500;
+        System.out.println(Search(vetor, fim, pivo, start, value));
     }
 
 
-    public static int BinSearch(int v[]){
+         public static int Search(int vetor[],int fim,int pivo, int start,int value){
+           
+           if(fim == 0){
+               return -1;
+           }
+                               
+            pivo =  (start + fim)/2;
+            
+ 
+              if(vetor[pivo] == value){
+                  return pivo;
+              }
+               else if(vetor[pivo]< value){
+                   start = pivo +1;
+              }
+              else if(vetor[pivo]>value){
+                   fim = pivo -1;
+              }
+     
 
-              
-     
-     
-     
-     
-        return 1;
-
-
-    }
+             return Search(vetor, fim,pivo,start,value);
+         }
     
 }
